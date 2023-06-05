3e420ac8d8f7b79a12ce615e380d27588bd8105b
Axel Padilla <adpadillar25@gmail.com>
2023-06-05 08:07:41 -0600

    styling /wallet

--- src/pages/wallet.tsx
+++ src/pages/wallet.tsx
@@ -32,18 +32,40 @@ const WalletPage: NextPage<WalletPageProps> = () => {
   }, [publicKey, loading, user, router]);
 
   return (
-    <div>
-      <h1>This is where you add your wallet</h1>
-      {!loading && user && (
-        <>
-          <div>
-            <WalletMultiButton />
-            <WalletDisconnectButton />
-          </div>
-          <pre>{JSON.stringify(user, null, 2)}</pre>
-        </>
-      )}
-      <pre>{publicKey && JSON.stringify(publicKey.toString(), null, 2)}</pre>
+    <div
+      className="min-h-screen"
+      style={{
+        backgroundColor: "#5df1b3",
+        backgroundImage:
+          "linear-gradient(55deg, #5df1b3 0%, #5df1b3 22%, #6cbefe 49%, #008fff 75%)",
+      }}
+    >
+      <div className="flex min-h-screen w-full flex-col items-center space-y-10 bg-white/[0.35] px-8">
+        <h1 className="max-w-md pt-24 text-center text-5xl font-semibold leading-[125%] tracking-wide text-prussian-blue">
+          Add your wallet
+        </h1>
+        <p className="text-center">
+          <strong>HACKATHON NOTE:</strong> This is a temporary feature. To make
+          it as easy as possible for a user to get started using crypto, we
+          intend to create a wallet for them automatically. This is too
+          difficult to do in little time, so we are asking you to add your own
+          wallet to the app.
+        </p>
+
+        <p>
+          <strong>
+            In the final product, THIS STEP WOULD BE COMPLETELY SKIPPED
+          </strong>
+        </p>
+        {!loading && user && (
+          <>
+            <div>
+              <WalletMultiButton />
+            </div>
+          </>
+        )}
+        <pre>{publicKey && JSON.stringify(publicKey.toString(), null, 2)}</pre>
+      </div>
     </div>
   );
 };
